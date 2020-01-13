93583417dd1875d17bd934e8378f7d2b3b90dffd
Valentyn Yuriev <valentyn_yuriev@epam.com>
2020-01-13 23:31:53 +0200

    Course Project Shop

---
+++ src/main/java/com/shopproject/valekedu/ShopApplication.java
@@ -0,0 +1,11 @@
+package com.shopproject.valekedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shopproject/valekedu/ValekeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopproject.valekedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValekeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
